2da6576cf983fdd176300899699e6032d688ca95
fazilsourcecode <mohamedfazil0705@gmail.com>
2024-07-03 16:02:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,12 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Mohamed Fazil",
+  title: "Software Engineer",
+  email: "mohamedfazil0705@gmail.com",
+  gitHub: "fazilsourcecode",
+ linkedIn: "Mohamed-mdfazil",
+  
 };
 
 const primaryColor = "#4E567E";
